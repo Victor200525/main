597205d7fdca2c060c42f8b10785876f95129c48
Pepsi1917 <pepsi1917@gmail.com>
2025-08-13 19:21:26 +0000

    Full Pipeline

--- pipeline.py
+++ pipeline.py
@@ -14,7 +14,7 @@ new_data = False # просто индикатор нам обрабатыват
 
 if new_data: 
     set_sentiment() # parsing json file, save to delta STAGING table
-#btc_quotes = eod_btc_quotes() # get quotes from Yahoo
-#df_sentiment = group_by_date()
-#join_sentiment_price(df_sentiment, btc_quotes) # JOIN dataframes
+btc_quotes = eod_btc_quotes() # get quotes from Yahoo
+df_sentiment = group_by_date()
+join_sentiment_price(df_sentiment, btc_quotes) # JOIN dataframes
 plt.plot_data()
