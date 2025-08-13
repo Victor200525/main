f8e4824d701ed45f2f1e9730724fd5229ca23d13
Pepsi1917 <pepsi1917@gmail.com>
2025-08-13 13:31:09 +0000

    Pipeline Folder

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,12 @@
 from pipeline.join_sentiment_price import join_sentiment_price
+from pipeline.download_yh_price_data import eod_btc_quotes
+from pipeline.set_sentiment_score import set_sentiment
+from pipeline.group_by_date import group_by_date
 
 # step-by-step
 # вызываем функции обратобки из каталога pipeline 
 
-join_sentiment_price()
+set_sentiment() # parsing json file, save to delta table
+eod_btc_quotes() # get quotes from Yahoo
+group_by_date()
+join_sentiment_price() # JOIN dataframes 
